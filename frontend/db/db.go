3a4e902c3972949506e9459112dcb03f04597acc
Marcus Puckett <mpuckett259@gmail.com>
2021-04-10 19:38:46 -0700

    First try (broken)
    This is the first try, I'm basically just saving this because it 100% does not work right now. Also want to be able to share with others to ask for help with this.

---
+++ frontend/db/db.go
@@ -0,0 +1,123 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/google/uuid"
+)
+
+type User struct {
+	SpeakerPostition	int16	`json:"speakerPosition"`
+	Name				string	`json:"name"`
+}
+
+func Start() {
+	// Delete and recreate existing sqlite file just in case
+	os.Remove("sqlite-database.db")
+	log.Println("Creating sqlite-database.db...")
+	file, err := os.Create("sqlite-database.db")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	file.Close()
+	log.Println("sqlite-database.db created")
+}
+
+func CreateTable() (newTableId string, err error) {
+	// Get sqlite db connection
+	sqliteDatabase, _ := sql.Open("sqlite3", "./sqlite-database.db")
+	defer sqliteDatabase.Close()
+
+	// Prepare table creation SQL
+	newTableId = uuid.New().String()
+	createMeetingTableSQL := `CREATE TABLE ? (
+		"idSpeaker" integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+		"name" TEXT
+	  );`
+	log.Println("Creating new meeting table with id " + newTableId + " ...")
+	statement, err := sqliteDatabase.Prepare(createMeetingTableSQL)
+	if err != nil {
+		log.Fatal(err.Error())
+		return "", err
+	}
+	defer statement.Close()
+
+	// Execute new table creation
+	statement.Exec(newTableId)
+	log.Println("Meeting table created successfully!")
+
+	// Return the new table ID
+	return newTableId, nil
+}
+
+func GetOnStack(tableId string, name string) (err error) {
+	// Get sqlite db connection
+	sqliteDatabase, _ := sql.Open("sqlite3", "./sqlite-database.db")
+	defer sqliteDatabase.Close()
+
+	// Prepare table update SQL
+	addUserToStackTableSQL := "INSERT INTO ? (name) VALUES (?);"
+	log.Println("Adding " + name + " to stack " + tableId)
+	statement, err := sqliteDatabase.Prepare(addUserToStackTableSQL)
+	if err != nil {
+		log.Fatal(err.Error())
+		return err
+	}
+	defer statement.Close()
+
+	// Execute new table update
+	statement.Exec(tableId, name)
+
+	// Return nothing because there are no failures
+	return nil
+}
+
+func GetOffStack(tableId string, name string) (err error) {
+	// Get sqlite db connection
+	sqliteDatabase, _ := sql.Open("sqlite3", "./sqlite-database.db")
+	defer sqliteDatabase.Close()
+
+	// Prepare table update SQL
+	removeUserFromStackTableSQL := "DELETE FROM ? WHERE name=?;"
+	log.Println("Removing " + name + " to stack " + tableId)
+	statement, err := sqliteDatabase.Prepare(removeUserFromStackTableSQL)
+	if err != nil {
+		log.Fatal(err.Error())
+		return err
+	}
+	defer statement.Close()
+
+	// Execute new table update
+	statement.Exec(tableId, name)
+
+	// Return nothing because there are no failures
+	return nil
+}
+
+func ShowCurrentStack(tableId string) (stackUsers []User, err error) {
+	// Get sqlite db connection
+	sqliteDatabase, _ := sql.Open("sqlite3", "./sqlite-database.db")
+	defer sqliteDatabase.Close()
+
+	// Prepare SELECT query
+	showCurrentStackTableSQL := "SELECT idSpeaker, name FROM ?;"
+	rows, err := sqliteDatabase.Query(showCurrentStackTableSQL, tableId)
+	if err != nil {
+		log.Fatal(err.Error())
+		return nil, err
+	}
+	defer rows.Close()
+
+	// Parse database rows to User object slice 
+	for rows.Next() {
+		var stackUser User
+		rows.Scan(&stackUser)
+		stackUsers = append(stackUsers, stackUser)
+	}
+
+	// Return current stack
+	return stackUsers, nil
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module stack-web-app
+
+go 1.15
+
+require (
+	github.com/google/uuid v1.2.0
+	github.com/gorilla/mux v1.8.0
+	github.com/gorilla/websocket v1.4.2
+	github.com/mattn/go-sqlite3 v1.14.6
+	github.com/zclconf/go-cty v1.8.1
+)
