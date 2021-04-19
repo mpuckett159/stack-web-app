8901e4322b3226bc2483f7b7c9546b74d6979b0e
Marcus Puckett <mpuckett259@gmail.com>
2021-04-19 12:17:13 -0700

    Maybe DO App platform now?

--- frontend/main.go
+++ frontend/main.go
@@ -35,17 +35,6 @@ func main() {
 		port = "80"
 	}
 
-	for _, encodedRoute := range strings.Split(os.Getenv("ROUTES"), ",") {
-		if encodedRoute == "" {
-			continue
-		}
-		pathAndBody := strings.SplitN(encodedRoute, "=", 2)
-		path, body := pathAndBody[0], pathAndBody[1]
-		http.HandleFunc("/"+path, func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprint(w, body)
-		})
-	}
-
 	bindAddr := fmt.Sprintf(":%s", port)
 	fmt.Println()
 	fmt.Printf("==> Server listening at %s 🚀\n", bindAddr)
